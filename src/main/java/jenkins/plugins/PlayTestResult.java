7c206062713f2bb4dd21fe5cc19530685f53d36f
Rafael Rezende <rafaelrezend@gmail.com>
2014-04-25 13:09:57 +0200

    Removed old implementation.

--- src/main/java/jenkins/plugins/PlayTestResult.java
+++
@@ -1,8 +0,0 @@
-package jenkins.plugins;
-
-public class PlayTestResult {
-	public String uri;
-	public String name;
-	public String status;
-
-}
---
+++ src/test/java/jenkins/plugins/PlayCommandGenerationTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package jenkins.plugins;
+
+import org.junit.Test;
+
+/**
+ * @author rafaelrezende
+ * 
+ */
+public class PlayCommandGenerationTest {
+
+	@Test
+	public void playRun() throws Exception {
+
+		
+	}
+}
