b55fb5f2139b48bac348815bc5766761922c5ef1
Rafael Rezende <rafaelrezend@gmail.com>
2014-05-13 00:07:13 +0200

    Version regex replaced by dotted-style ranges.

--- src/main/java/jenkins/plugins/play/PlayTarget.java
+++ src/main/java/jenkins/plugins/play/PlayTarget.java
@@ -4,28 +4,38 @@
 package jenkins.plugins.play;
 
 /**
- * @author rafaelrezende
+ * Play targets are used to identify which commands are available for a provided
+ * Play! installation in the job configuration of Jenkins. Every play target has
+ * a range of versions and a specific executable name.
  * 
+ * For instance, if the selected Play! installation is identified to be "2.2.1",
+ * it will fit in the range of 2-2.2 below and the 'play' executable will be
+ * used.
+ * A range "1-1" includes every version starting with 1 (i.e.: '1.2.3')
  */
 public enum PlayTarget {
 
 	// Default target
-	NONE("", ""),
+	NONE("", "0.0.0", "0.0.0"),
 
-	// ERROR MESSAGES
-	PLAY_1_X("play", "1."),
-	PLAY_2_X("play", "2.2."),
-	PLAY_2_3("activator", "2.2.3");
+	// CUSTOM TARGETS
+	// Any version started with "1."
+	PLAY_1_X("play", "1", "1"),
+	// Any version started with "2.x.", with x from 0 to 2
+	PLAY_2_X("play", "2", "2.2"),
+	// Any version started with "2.x." with x from 3 to 99.
+	PLAY_2_3("activator", "2.3", "2.3");
 
 	/**
-	 * Play executable. (i.e.: 'play' from first version until v2.2.3. Then 'activator')
+	 * Play executable. (i.e.: 'play' from first version until v2.2.3. Then
+	 * 'activator')
 	 */
 	private String executable;
-	/**
-	 * Regular expression to identify the Play! version
-	 */
-	private String versionRegex;
+
+	private String versionRangeMin;
 	
+	private String versionRangeMax;
+
 	/**
 	 * @return the executable
 	 */
@@ -34,25 +44,33 @@ public enum PlayTarget {
 	}
 
 	/**
-	 * @return the versionRegex
+	 * @return the versionRangeMin
 	 */
-	public final String getVersionRegex() {
-		return versionRegex;
+	public final String getVersionRangeMin() {
+		return versionRangeMin;
+	}
+
+	/**
+	 * @return the versionRangeMax
+	 */
+	public final String getVersionRangeMax() {
+		return versionRangeMax;
 	}
 
 	/**
 	 * Private constructor. No external object can create targets.
 	 * 
-	 * @param code 
+	 * @param code
 	 * @param message
 	 */
-	private PlayTarget(String code, String message) {
+	private PlayTarget(String code, String versionRangeMin, String versionRangeMax) {
 		this.executable = code;
-		this.versionRegex = message;
+		this.versionRangeMin = versionRangeMin;
+		this.versionRangeMax = versionRangeMax;
 	}
 
 	@Override
 	public String toString() {
-		return executable + ": " + versionRegex;
+		return executable + ": " + versionRangeMin;
 	}
 }
